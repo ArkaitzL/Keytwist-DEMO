e527c30535e6edcd75435410f006ffc44c5c783c
ArkaitzL <127892239+ArkaitzL@users.noreply.github.com>
2024-01-09 13:27:18 +0100

    -

--- Assets/Controlador.cs
+++ Assets/Controlador.cs
@@ -1,20 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using BaboOnLite;
 
 public class Controlador : MonoBehaviour
 {
-    [SerializeField] Teclado tecladoL, tecladoR;
+    [SerializeField] Teclado[] teclados;
     public static Controlador instancia;
+
+    int actual = 0;
 
     private void Start()
     {
         instancia = this;
+
+        actual = Random.Range(0, 2);
         Nueva();
     }
 
     public void Nueva() 
     {
-        tecladoL.Nueva(tecladoL.teclas[Random.Range(0, tecladoL.teclas.Length)].codigo);
+        teclados[actual].Nueva(teclados[actual].teclas[Random.Range(0, teclados[actual].teclas.Length)].codigo);
+        actual = (actual == 0) ? 1 : 0;
     }
 }
